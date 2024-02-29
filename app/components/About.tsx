091d5fb5e7f1245915650fb88a705081bd1c702b
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-29 20:49:23 +0530

    animation started

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,40 +6,60 @@ import { useEffect, useRef } from "react";
 import { data } from "../lib/data";
 
 export const About = () => {
-	const imageRef = useRef<HTMLImageElement>(null);
-	const containerRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		let ctx = gsap.context(() => {
-			gsap.set(imageRef.current, { y: -900, x: 900 });
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.set(imageRef.current, { yPercent: -200, xPercent: 10, scale: 0.5 });
 
-			gsap.to(imageRef.current, {
-				y: 0,
-				x: 0,
-				scrollTrigger: {
-					trigger: containerRef.current,
-					start: "top center",
-					end: "top top",
-					scrub: 1,
-					markers: true,
-				},
-			});
-		}, containerRef);
-	}, []);
+      const timeline = gsap.timeline();
+      timeline
+        .to(imageRef.current, {
+          yPercent: -50,
+          xPercent: 0,
+          scale: 1,
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: "top bottom",
+            end: "top 60%",
+            scrub: 1,
+            markers: true,
+          },
+        })
+        .fromTo(
+          imageRef.current,
+          { yPercent: -50, xPercent: 0, scale: 1 },
+          {
+            immediateRender: false,
+            yPercent: 0,
+            xPercent: 0,
+            scale: 1.5,
+            scrollTrigger: {
+              trigger: containerRef.current,
+              start: "top 40%",
+              end: "bottom bottom",
+              scrub: 1,
+              markers: true,
+            },
+          },
+        );
+    }, containerRef);
+  }, []);
 
-	return (
-		<div
-			ref={containerRef}
-			className="h-screen w-full flex items-center justify-center"
-		>
-			<Image
-				ref={imageRef}
-				src={data.personal.profileImg}
-				alt=""
-				height={1000}
-				width={1000}
-				className="object-cover h-[500px] w=[500px] rounded-3xl"
-			/>
-		</div>
-	);
+  return (
+    <div
+      ref={containerRef}
+      className="h-screen w-full flex items-center justify-center"
+    >
+      <Image
+        ref={imageRef}
+        src={data.personal.profileImg}
+        alt=""
+        height={1000}
+        width={1000}
+        className="object-cover h-[500px] w=[500px] rounded-3xl"
+      />
+    </div>
+  );
 };
