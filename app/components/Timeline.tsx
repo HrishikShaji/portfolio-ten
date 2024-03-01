9cfd6adb663f040214e79984d1cc7a17d2be456d
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-01 17:00:21 +0530

    added heading animation

--- app/components/Timeline.tsx
+++ app/components/Timeline.tsx
@@ -3,88 +3,79 @@ import { data } from "../lib/data";
 import gsap from "gsap";
 
 export const Timeline = () => {
-	const itemRefs = useRef<(HTMLDivElement | null)[]>([]);
-	const containerRef = useRef<HTMLDivElement>(null);
+  const itemRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const educationRef = useRef<HTMLHeadingElement>(null);
+  const experienceRef = useRef<HTMLHeadingElement>(null);
 
-	const coordinates = [
-		{
-			xPercent: 0,
-			yPercent: 0,
-		},
-		{
-			xPercent: -100,
-			yPercent: 0,
-		},
-		{
-			xPercent: 100,
-			yPercent: 0,
-		},
-		{
-			xPercent: 0,
-			yPercent: -100,
-		},
-		{
-			xPercent: 0,
-			yPercent: -100,
-		},
-		{
-			xPercent: -100,
-			yPercent: 0,
-		},
-	];
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.set(educationRef.current, { xPercent: -100 });
+      gsap.set(experienceRef.current, { xPercent: 100 });
 
-	useEffect(() => {
-		console.log(itemRefs);
-		let ctx = gsap.context(() => {
-			gsap.set(itemRefs.current, {
-				yPercent: (i) => coordinates[i].yPercent,
-				xPercent: (i) => coordinates[i].xPercent,
-			});
-			gsap.to(itemRefs.current, {
-				yPercent: () => 0,
-				xPercent: () => 0,
-				stagger: 0.1,
-				scrollTrigger: {
-					trigger: containerRef.current,
-					start: "top center",
-					end: "top top",
-					scrub: true,
-					markers: true,
-				},
-			});
-		}, containerRef);
+      const timeline = gsap.timeline();
 
-		return () => ctx.kill();
-	}, []);
-	return (
-		<div
-			ref={containerRef}
-			className="h-screen w-full flex items-center justify-center text-white"
-		>
-			<div className="grid grid-cols-2  w-[75%] h-[50%]">
-				{data.education.data.map((item, i) => (
-					<div
-						className="p-5  "
-						key={item.id}
-						ref={(el) => (itemRefs.current[item.id] = el)}
-					>
-						<div className="bg-neutral-500 w-full h-full rounded-3xl flex items-center justify-center">
-							<h1>{item.major}</h1>
-						</div>
-					</div>
-				))}
-				{data.experience.data.map((item, i) => (
-					<div
-						className="p-5 "
-						key={item.id}
-						ref={(el) => (itemRefs.current[item.id] = el)}
-					>
-						<div className="bg-neutral-500 w-full h-full rounded-3xl flex items-center justify-center">
-							<h1>{item.company}</h1>
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+      timeline
+        .to(educationRef.current, {
+          xPercent: 0,
+          scrollTrigger: {
+            trigger: educationRef.current,
+            start: "top 75%",
+            end: "top top",
+            scrub: 1,
+          },
+        })
+        .to(experienceRef.current, {
+          xPercent: 0,
+          scrollTrigger: {
+            trigger: experienceRef.current,
+            start: "top 75%",
+            end: "top top",
+            scrub: 1,
+          },
+        });
+    }, containerRef);
+
+    return () => ctx.kill();
+  }, []);
+  return (
+    <div
+      ref={containerRef}
+      className="h-screen w-full flex flex-col items-center justify-center text-white"
+    >
+      <div className="flex gap-10 items-center">
+        <h1 className="text-8xl font-semibold" ref={educationRef}>
+          Education
+        </h1>
+        <h1 className="text-9xl font-semibold">&</h1>
+        <h1 className="text-8xl font-semibold" ref={experienceRef}>
+          Experience
+        </h1>
+      </div>
+      <div className="grid grid-cols-2  w-[75%] h-[50%]">
+        {data.education.data.map((item, i) => (
+          <div
+            className="p-5  "
+            key={item.id}
+            ref={(el) => (itemRefs.current[item.id] = el)}
+          >
+            <div className="bg-neutral-500 w-full h-full rounded-3xl flex items-center justify-center">
+              <h1>{item.major}</h1>
+            </div>
+          </div>
+        ))}
+        {data.experience.data.map((item, i) => (
+          <div
+            className="p-5 "
+            key={item.id}
+            ref={(el) => (itemRefs.current[item.id] = el)}
+          >
+            <div className="bg-neutral-500 w-full h-full rounded-3xl flex items-center justify-center">
+              <h1>{item.company}</h1>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
