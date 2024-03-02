c631d7206514f16d8b02a14424fdf934dfb77382
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-02 22:28:00 +0530

    added accordion animation

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -4,20 +4,24 @@ import gsap from "gsap";
 
 interface AccordionProps {
   items: any[];
+  active: number;
 }
 
-export const Accordion: React.FC<AccordionProps> = ({ items }) => {
+export const Accordion: React.FC<AccordionProps> = ({ items, active }) => {
   const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     let ctx = gsap.context(() => {
+      gsap.set(projectRefs.current, {
+        width: (i) => (i === active ? "600px" : "200px"),
+      });
       projectRefs.current.forEach((el) => {
         const width = el?.getBoundingClientRect().width;
+
         const mouseEnter = (e: MouseEvent) => {
           gsap.to(projectRefs.current, {
-            width: (i) =>
-              projectRefs.current[i] === el ? width * 5 : width * 0.2,
+            width: (i) => (projectRefs.current[i] === el ? "600px" : "200px"),
           });
         };
         const mouseLeave = (e: MouseEvent) => {
@@ -26,7 +30,6 @@ export const Accordion: React.FC<AccordionProps> = ({ items }) => {
           });
         };
         el?.addEventListener("mouseenter", mouseEnter);
-        el?.addEventListener("mouseleave", mouseLeave);
 
         return () => {
           el?.removeEventListener("mouseenter", mouseEnter);
@@ -36,14 +39,14 @@ export const Accordion: React.FC<AccordionProps> = ({ items }) => {
     }, containerRef);
 
     return () => ctx.kill();
-  }, []);
+  }, [active]);
   return (
     <div ref={containerRef} className="flex gap-2 w-full h-full">
       {items.map((item, i) => (
         <div
           key={i}
           ref={(el) => (projectRefs.current[i] = el)}
-          className="bg-white rounded-3xl flex w-full items-center justify-center"
+          className="bg-white rounded-3xl flex  items-center justify-center"
         >
           <h1>{i}</h1>
         </div>
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -4,10 +4,10 @@ import { Accordion } from "./Accordion";
 export const Projects = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className="w-[80%] h-[80%]  flex flex-col gap-2">
-        <Accordion items={data.projects.data.slice(0, 4)} />
-        <Accordion items={data.projects.data.slice(4, 8)} />
-        <Accordion items={data.projects.data.slice(8, 12)} />
+      <div className=" flex h-[75%] flex-col gap-2">
+        <Accordion active={0} items={data.projects.data.slice(0, 4)} />
+        <Accordion active={1} items={data.projects.data.slice(4, 8)} />
+        <Accordion active={2} items={data.projects.data.slice(8, 12)} />
       </div>
     </div>
   );
