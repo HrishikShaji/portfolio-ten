84942c1ec409435daca8e5e33c5660098f50c7f9
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-03 17:46:34 +0530

    removed type errore

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -1,91 +1,94 @@
-import { Ref, forwardRef, useEffect, useRef, useState } from "react";
-import { data } from "../lib/data";
+import { MutableRefObject, forwardRef, useEffect, useRef } from "react";
 import gsap from "gsap";
 
 interface AccordionProps {
-  items: any[];
-  active: number;
+	items: any[];
+	active: number;
 }
 
 const Accordion = forwardRef<HTMLDivElement, AccordionProps>(
-  ({ items, active }, ref) => {
-    const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
-    const containerRef = useRef<HTMLDivElement>(null);
-    console.log(ref);
+	({ items, active }, ref) => {
+		const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
+		const containerRef = useRef<HTMLDivElement>(null);
+		const parentRef = ref as MutableRefObject<HTMLDivElement>;
 
-    useEffect(() => {
-      let ctx = gsap.context(() => {
-        const timeline = gsap.timeline();
+		useEffect(() => {
+			let ctx = gsap.context(() => {
+				const timeline = gsap.timeline();
 
-        timeline
-          .fromTo(
-            projectRefs.current,
-            {
-              xPercent: -100,
-            },
-            {
-              xPercent: 0,
-              stagger: 0.25,
-              scrollTrigger: {
-                trigger: containerRef.current,
-                start: "-50% 80%",
-                end: () => `+=${projectRefs.current[0]?.offsetHeight}`,
-                scrub: 1,
-              },
-            },
-          )
-          .fromTo(
-            projectRefs.current,
-            { width: "200px" },
-            {
-              duration: 0.5,
-              width: (i) => (i === active ? "600px" : "200px"),
-              scrollTrigger: {
-                trigger: containerRef.current,
-                start: "top 80%",
-                markers: true,
-              },
-            },
-          );
+				timeline
+					.fromTo(
+						projectRefs.current,
+						{
+							xPercent: -100,
+						},
+						{
+							xPercent: 0,
+							stagger: {
+								from: "end",
+								each: 0.2,
+							},
+							scrollTrigger: {
+								trigger: containerRef.current,
+								start: "-50% 80%",
+								end: () => `+=${projectRefs.current[0]?.offsetHeight}`,
+								scrub: 1,
+							},
+						},
+					)
+					.fromTo(
+						projectRefs.current,
+						{ width: "200px" },
+						{
+							duration: 0.5,
+							width: (i) => (i === active ? "600px" : "200px"),
+							scrollTrigger: {
+								trigger: parentRef.current,
+								start: "top 20%",
+								end: "top top",
+								markers: true,
+							},
+						},
+					);
 
-        projectRefs.current.forEach((el) => {
-          const width = el?.getBoundingClientRect().width;
+				projectRefs.current.forEach((el) => {
+					const width = el?.getBoundingClientRect().width;
 
-          const mouseEnter = (e: MouseEvent) => {
-            gsap.to(projectRefs.current, {
-              width: (i) => (projectRefs.current[i] === el ? "600px" : "200px"),
-            });
-          };
-          const mouseLeave = (e: MouseEvent) => {
-            gsap.to(projectRefs.current, {
-              width: width,
-            });
-          };
-          el?.addEventListener("mouseenter", mouseEnter);
+					const mouseEnter = (e: MouseEvent) => {
+						gsap.to(projectRefs.current, {
+							width: (i) => (projectRefs.current[i] === el ? "600px" : "200px"),
+						});
+					};
+					const mouseLeave = (e: MouseEvent) => {
+						gsap.to(projectRefs.current, {
+							width: width,
+						});
+					};
+					el?.addEventListener("mouseenter", mouseEnter);
 
-          return () => {
-            el?.removeEventListener("mouseenter", mouseEnter);
-            el?.removeEventListener("mouseleave", mouseLeave);
-          };
-        });
-      }, containerRef);
+					return () => {
+						el?.removeEventListener("mouseenter", mouseEnter);
+						el?.removeEventListener("mouseleave", mouseLeave);
+					};
+				});
+			}, containerRef);
 
-      return () => ctx.kill();
-    }, [active]);
-    return (
-      <div ref={containerRef} className="flex gap-2 w-full h-full">
-        {items.map((item, i) => (
-          <div
-            key={i}
-            ref={(el) => (projectRefs.current[i] = el)}
-            className="bg-white rounded-3xl flex  items-center justify-center"
-          >
-            <h1>{i}</h1>
-          </div>
-        ))}
-      </div>
-    );
-  },
+			return () => ctx.kill();
+		}, []);
+		return (
+			<div ref={containerRef} className="flex gap-2 w-full h-full">
+				{items.map((item, i) => (
+					<div
+						key={i}
+						ref={(el) => (projectRefs.current[i] = el)}
+						className="bg-white rounded-3xl flex  items-center justify-center"
+					>
+						<h1>{i}</h1>
+					</div>
+				))}
+			</div>
+		);
+	},
 );
 
 Accordion.displayName = "Accordion";
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,27 +3,30 @@ import { data } from "../lib/data";
 import Accordion from "./Accordion";
 
 export const Projects = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
 
-  return (
-    <div className="h-screen w-full flex justify-center items-center">
-      <div className=" flex h-[75%] flex-col gap-2" ref={containerRef}>
-        <Accordion
-          active={0}
-          items={data.projects.data.slice(0, 4)}
-          ref={containerRef}
-        />
-        <Accordion
-          active={1}
-          items={data.projects.data.slice(4, 8)}
-          ref={containerRef}
-        />
-        <Accordion
-          active={2}
-          items={data.projects.data.slice(8, 12)}
-          ref={containerRef}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div
+			ref={containerRef}
+			className="h-screen w-full flex justify-center items-center"
+		>
+			<div className=" flex h-[75%] flex-col gap-2">
+				<Accordion
+					active={0}
+					items={data.projects.data.slice(0, 4)}
+					ref={containerRef}
+				/>
+				<Accordion
+					active={1}
+					items={data.projects.data.slice(4, 8)}
+					ref={containerRef}
+				/>
+				<Accordion
+					active={2}
+					items={data.projects.data.slice(8, 12)}
+					ref={containerRef}
+				/>
+			</div>
+		</div>
+	);
 };
