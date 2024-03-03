49a76d3202b1bf862ca73c42688bc04676f9986a
HrishikShaji <hrishikminishaji@gmail.com>
2024-03-03 12:13:21 +0530

    added forwardRef

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { Ref, forwardRef, useEffect, useRef, useState } from "react";
 import { data } from "../lib/data";
 import gsap from "gsap";
 
@@ -7,50 +7,87 @@ interface AccordionProps {
   active: number;
 }
 
-export const Accordion: React.FC<AccordionProps> = ({ items, active }) => {
-  const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const containerRef = useRef<HTMLDivElement>(null);
+const Accordion = forwardRef<HTMLDivElement, AccordionProps>(
+  ({ items, active }, ref) => {
+    const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
+    const containerRef = useRef<HTMLDivElement>(null);
+    console.log(ref);
 
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      gsap.set(projectRefs.current, {
-        width: (i) => (i === active ? "600px" : "200px"),
-      });
-      projectRefs.current.forEach((el) => {
-        const width = el?.getBoundingClientRect().width;
+    useEffect(() => {
+      let ctx = gsap.context(() => {
+        const timeline = gsap.timeline();
 
-        const mouseEnter = (e: MouseEvent) => {
-          gsap.to(projectRefs.current, {
-            width: (i) => (projectRefs.current[i] === el ? "600px" : "200px"),
-          });
-        };
-        const mouseLeave = (e: MouseEvent) => {
-          gsap.to(projectRefs.current, {
-            width: width,
-          });
-        };
-        el?.addEventListener("mouseenter", mouseEnter);
+        timeline
+          .fromTo(
+            projectRefs.current,
+            {
+              xPercent: -100,
+            },
+            {
+              xPercent: 0,
+              stagger: 0.25,
+              scrollTrigger: {
+                trigger: containerRef.current,
+                start: "-50% 80%",
+                end: () => `+=${projectRefs.current[0]?.offsetHeight}`,
+                scrub: 1,
+              },
+            },
+          )
+          .fromTo(
+            projectRefs.current,
+            { width: "200px" },
+            {
+              duration: 0.5,
+              width: (i) => (i === active ? "600px" : "200px"),
+              scrollTrigger: {
+                trigger: containerRef.current,
+                start: "top 80%",
+                markers: true,
+              },
+            },
+          );
 
-        return () => {
-          el?.removeEventListener("mouseenter", mouseEnter);
-          el?.removeEventListener("mouseleave", mouseLeave);
-        };
-      });
-    }, containerRef);
+        projectRefs.current.forEach((el) => {
+          const width = el?.getBoundingClientRect().width;
 
-    return () => ctx.kill();
-  }, [active]);
-  return (
-    <div ref={containerRef} className="flex gap-2 w-full h-full">
-      {items.map((item, i) => (
-        <div
-          key={i}
-          ref={(el) => (projectRefs.current[i] = el)}
-          className="bg-white rounded-3xl flex  items-center justify-center"
-        >
-          <h1>{i}</h1>
-        </div>
-      ))}
-    </div>
-  );
-};
+          const mouseEnter = (e: MouseEvent) => {
+            gsap.to(projectRefs.current, {
+              width: (i) => (projectRefs.current[i] === el ? "600px" : "200px"),
+            });
+          };
+          const mouseLeave = (e: MouseEvent) => {
+            gsap.to(projectRefs.current, {
+              width: width,
+            });
+          };
+          el?.addEventListener("mouseenter", mouseEnter);
+
+          return () => {
+            el?.removeEventListener("mouseenter", mouseEnter);
+            el?.removeEventListener("mouseleave", mouseLeave);
+          };
+        });
+      }, containerRef);
+
+      return () => ctx.kill();
+    }, [active]);
+    return (
+      <div ref={containerRef} className="flex gap-2 w-full h-full">
+        {items.map((item, i) => (
+          <div
+            key={i}
+            ref={(el) => (projectRefs.current[i] = el)}
+            className="bg-white rounded-3xl flex  items-center justify-center"
+          >
+            <h1>{i}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  },
+);
+
+Accordion.displayName = "Accordion";
+
+export default Accordion;
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,13 +1,28 @@
+import { useRef } from "react";
 import { data } from "../lib/data";
-import { Accordion } from "./Accordion";
+import Accordion from "./Accordion";
 
 export const Projects = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <div className=" flex h-[75%] flex-col gap-2">
-        <Accordion active={0} items={data.projects.data.slice(0, 4)} />
-        <Accordion active={1} items={data.projects.data.slice(4, 8)} />
-        <Accordion active={2} items={data.projects.data.slice(8, 12)} />
+      <div className=" flex h-[75%] flex-col gap-2" ref={containerRef}>
+        <Accordion
+          active={0}
+          items={data.projects.data.slice(0, 4)}
+          ref={containerRef}
+        />
+        <Accordion
+          active={1}
+          items={data.projects.data.slice(4, 8)}
+          ref={containerRef}
+        />
+        <Accordion
+          active={2}
+          items={data.projects.data.slice(8, 12)}
+          ref={containerRef}
+        />
       </div>
     </div>
   );
